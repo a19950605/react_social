d1c247d20e0fdf690b1536742572bb497655ff88
wuchau <wuchau@connect.ust.hk>
2022-06-14 23:11:30 +0800

    sidebar

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,15 @@
+import './share.css';
+
+const Share = () => {
+	return (
+		<div className="share">
+			<div className="shareWrapper">
+				<div className="shareTop"></div>
+
+				<div className="shareTop"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Share;
